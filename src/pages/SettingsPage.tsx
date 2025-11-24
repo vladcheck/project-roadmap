b739154814ceee76041ab4ad79469ac3931a5c5b
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-11-25 00:00:24 +0300

    feat: add a form to settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import Row from "../components/Row/Row";
 import useLocalStorage from "../hooks/useLocalStorage";
 import useTechnologies from "../hooks/useTechnologies";
@@ -12,53 +13,69 @@ export default function SettingsPage() {
     "notifications",
     true,
   );
+  const formRef = useRef<HTMLFormElement>(null);
 
   return (
     <section className="user-settings">
       <h2>Настройки пользователя</h2>
-      <Row>
-        <label>Имя пользователя:</label>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Введите ваше имя"
-        />
-      </Row>
-      <Row>
-        <label>Тема оформления:</label>
-        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
-          <option value="light">Светлая</option>
-          <option value="dark">Темная</option>
-          <option value="auto">Авто</option>
-        </select>
-      </Row>
-      <Row>
-        <label htmlFor="turn-on-notifications">Включить уведомления</label>
-        <input
-          id="turn-on-notifications"
-          name="turnOnNotifications"
-          type="checkbox"
-          checked={notifications}
-          onChange={(e) => setNotifications(e.target.checked)}
-        />
-      </Row>
-      <Row>
+      <form id="user-settings-form" ref={formRef}>
+        <Row>
+          <label>Имя пользователя:</label>
+          <input
+            type="text"
+            minLength={3}
+            maxLength={40}
+            pattern="[A-Za-z][\w]*"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Введите ваше имя"
+          />
+        </Row>
+        <Row>
+          <label>Тема оформления:</label>
+          <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+            <option value="light">Светлая</option>
+            <option value="dark">Темная</option>
+            <option value="auto">Авто</option>
+          </select>
+        </Row>
+        <Row>
+          <label htmlFor="turn-on-notifications">Включить уведомления</label>
+          <input
+            id="turn-on-notifications"
+            name="turnOnNotifications"
+            type="checkbox"
+            checked={notifications}
+            onChange={(e) => setNotifications(e.target.checked)}
+          />
+        </Row>
+        <Row>
+          <button
+            onClick={() => {
+              const answer = prompt(
+                "Вы точно этого хотите?",
+              )?.toLocaleLowerCase();
+              if (answer === "yes" || answer === "y" || answer === "да") {
+                setTechnologies([]);
+                alert("localStorage очищен.");
+              }
+            }}
+          >
+            Очистить <code>localStorage</code>
+          </button>
+        </Row>
         <button
-          onClick={() => {
-            const answer = prompt(
-              "Вы точно этого хотите?",
-            )?.toLocaleLowerCase();
-            if (answer === "yes" || answer === "y" || answer === "да") {
-              setTechnologies([]);
-              alert("localStorage очищен.");
+          type="submit"
+          onClick={(e) => {
+            if (formRef.current?.checkValidity()) {
+              e.preventDefault();
+              alert("[STUB] Успешно!");
             }
           }}
         >
-          Очистить <code>localStorage</code>
+          Сохранить изменения
         </button>
-      </Row>
-      <hr />
+      </form>
       <div className="current-settings">
         <h3>Текущие настройки</h3>
         <Row>
